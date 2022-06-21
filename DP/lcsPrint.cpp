d1a307707bb3f2ffe2cb2a810092225ab77b46a4
Abhishek Paul <abhishek.paulcp.dbs@gmail.com>
2022-06-21 21:55:35 +0530

    lcs print

--- DP/lcsPrint.cpp
+++ DP/lcsPrint.cpp
@@ -1,14 +1,10 @@
 #include <bits/stdc++.h>
+using namespace std;
 
-vector<vector<int>> lcs(string s, string t)
+void lcs(string &s, string &t, vector<vector<int>> &tab)
 {
     int m = s.length();
     int n = t.length();
-
-    vector<vector<int>> dp(m+1, vector<int> (n+1, -1));
-    return solve(m, n, s, t, dp);
-
-    vector<vector<int>> tab(m+1, vector<int> (n+1, 0));
 
     for(int i = 1; i <= m; i++) {
         for(int j = 1; j <= n; j++) {
@@ -19,8 +15,6 @@ vector<vector<int>> lcs(string s, string t)
             }
         }
     }
-
-    return tab;
 }
 
 int main() {
@@ -28,16 +22,17 @@ int main() {
   cout<<"Enter two strings: "<<endl;
   cin>>a>>b;
 
-  vector<vector<int>> tab = lcs(a, b);
-
   int i = a.length();
   int j = b.length();
 
-  string res = '';
+  vector<vector<int>> tab(i+1, vector<int> (j+1, 0));
+  lcs(a, b, tab);
 
-  while(i != 0 || j != 0) {
+  string res = "";
+
+  while(i != 0 && j != 0) {
     if(a[i-1] == b[j-1]) {
-      s += a[i-1];
+      res += a[i-1];
       i--;
       j--;
     } else {
